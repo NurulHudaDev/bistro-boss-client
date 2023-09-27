24d6e60f38446d5e5b0208f8cb3f7a3b6c18e870
NurulHudaDev <nurulhuda22dev@gmail.com>
2023-09-27 15:18:27 +0600

    our-menu

---
+++ src/Hooks/UesMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UesMenu = () => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default UesMenu;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Menu from "../Pages/Menu/Menu/Menu";
 
  export const router = createBrowserRouter([
     {
@@ -11,5 +12,9 @@ import Home from "../Pages/Home/Home/Home";
       children: [{
         path: '/', element: <Home></Home>
       }]
+    },
+    {
+      path: "menu",
+      element: <Menu></Menu>
     },
   ]);
